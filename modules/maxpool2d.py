866f0884ecdc360f8a07d6e50de95bce613e4082
Pame Ruiz <pameruiz_095@hotmail.com>
2026-03-21 16:24:03 +0100

    Optimizando maxpool2d

--- modules/maxpool2d.py
+++ modules/maxpool2d.py
@@ -7,7 +7,7 @@ class MaxPool2D(Layer):
         self.kernel_size = kernel_size
         self.stride = stride
 
-    def forward(self, input, training=True):  # input: np.ndarray of shape [B, C, H, W]
+    def forward_original(self, input, training=True):  # input: np.ndarray of shape [B, C, H, W]
         self.input = input
         B, C, H, W = input.shape
         KH, KW = self.kernel_size, self.kernel_size
@@ -36,8 +36,51 @@ class MaxPool2D(Layer):
                         self.max_indices[b, c, i, j] = (h_start + max_idx[0], w_start + max_idx[1])
 
         return output
+    
+    ## ----- Generado con IA
+    def forward(self, input, training=True):
+        self.input = input
+        B, C, H, W = input.shape
+        KH, KW = self.kernel_size, self.kernel_size
+        SH, SW = self.stride, self.stride
+    
+        out_h = (H - KH) // SH + 1
+        out_w = (W - KW) // SW + 1
+    
+        # Crear vista de todas las ventanas simultáneamente
+        shape = (B, C, out_h, out_w, KH, KW)
+        strides = (
+            input.strides[0],
+            input.strides[1],
+            input.strides[2] * SH,
+            input.strides[3] * SW,
+            input.strides[2],
+            input.strides[3],
+        )
+        windows = np.lib.stride_tricks.as_strided(input, shape=shape, strides=strides)
+    
+        # Máximo sobre las dos últimas dimensiones (KH, KW) en una sola operación
+        output = windows.max(axis=(4, 5))
+    
+        # Índices del máximo (aplanados dentro de cada ventana)
+        flat_idx = windows.reshape(B, C, out_h, out_w, -1).argmax(axis=4)
+    
+        # Convertir índice plano → coordenadas (r, s) globales
+        local_r, local_s = np.unravel_index(flat_idx, (KH, KW))
+    
+        # Sumar el offset de cada ventana para obtener coordenadas globales
+        row_offsets = np.arange(out_h)[None, None, :, None] * SH
+        col_offsets = np.arange(out_w)[None, None, None, :] * SW
+    
+        self.max_indices = np.stack([
+            local_r + row_offsets,
+            local_s + col_offsets
+        ], axis=-1)
+    
+        return output
+## ---------- Fin generado con IA
 
-    def backward(self, grad_output, learning_rate=None):
+    def backward_oirignal(self, grad_output, learning_rate=None):
         B, C, H, W = self.input.shape
         grad_input = np.zeros_like(self.input, dtype=grad_output.dtype)
         out_h, out_w = grad_output.shape[2], grad_output.shape[3]
@@ -50,3 +93,24 @@ class MaxPool2D(Layer):
                         grad_input[b, c, r, s] += grad_output[b, c, i, j]
 
         return grad_input
+    ## ----- Generado con IA
+    def backward(self, grad_output):
+        B, C, out_h, out_w = grad_output.shape
+        grad_input = np.zeros_like(self.input)
+
+        # 1. Creamos índices para las dimensiones Batch y Canal
+        # Usamos np.arange y broadcasting para que coincidan con la forma de grad_output
+        b_idx = np.arange(B).reshape(B, 1, 1, 1)
+        c_idx = np.arange(C).reshape(1, C, 1, 1)
+        
+        # 2. Extraemos las coordenadas r y s guardadas en el forward
+        # Asumiendo que max_indices tiene forma (B, C, out_h, out_w, 2)
+        r_idx = self.max_indices[:, :, :, :, 0]
+        s_idx = self.max_indices[:, :, :, :, 1]
+
+        # 3. Dispersión del gradiente (Scatter)
+        # NumPy permite usar arrays de índices para asignar valores en bloque
+        np.add.at(grad_input, (b_idx, c_idx, r_idx, s_idx), grad_output)
+
+        return grad_input
+    ## ---------- Fin generado con IA
